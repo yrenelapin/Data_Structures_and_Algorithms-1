a54d1591921d495a1d3fe6a244fb7283d72b0914
Sanjay Marreddi <57671048+SanjayMarreddi@users.noreply.github.com>
2020-09-09 22:08:36 +0530

    Add files via upload

---
+++ Beginner/misc/ass1.cc
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+
+    float var = 100.3 ;
+
+    printf("var=%f",var);
+
+    // printf ("Hello");
+}
---
+++ Beginner/misc/ass2.cc
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+// float hai(){
+
+//     int hai ;
+//     hai = 10;
+//     printf("%d",hai) ;
+
+//     return 1.5; }
+
+
+// // A: Can we define two functions with same name but two different return types? NO
+
+// // B: Can we define a function and a variable with same name? YES
+
+
+//  void swap(int a, int b){int temp = a; b = a; a = temp;}
+
+// int x =0;
+ 
+// int main()
+// {
+//     hai();
+
+//     int a = 10;
+//     int b = 20;
+//     swap(a,b);
+//     printf("a=%d,b=%d",a,b); // No change in Values !!
+
+//     int x =20 ;
+//     x=x+1;
+//     printf("%d",x);     // 21
+
+// }
+
+
+
+char ch = 'a' ;
+int main(){
+
+//  if (ch == 'b') 
+//  printf("0");        // Yields 1
+//  else printf("1"); 
+
+ if (ch='b') printf("0");   // Yields 0
+ else printf("1"); 
+
+ }
+
