49b97d4f8316aaa93f00fec3d0698e4f1a416210
SanjayMarreddi <sanjay.mareddi@gmail.com>
2021-03-22 23:12:02 +0530

    Improved template a bit

--- Competitive_Programming/Library/basic_template.cpp
+++ Competitive_Programming/Library/basic_template.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define deb(x) cout << #x << " = " << x << endl
+#define deb2(x, y) cout << #x << " = " << x << "  ,  " << #y << "=" << y << endl
 
 int main() {
     ios_base::sync_with_stdio(0);
