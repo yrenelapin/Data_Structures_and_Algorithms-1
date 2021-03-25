a090c95e5f059ad791044e7f5b6fcae421da7d09
SanjayMarreddi <sanjay.mareddi@gmail.com>
2021-03-25 23:10:57 +0530

    Updated Template

--- Competitive_Programming/Library/basic_template.cpp
+++ Competitive_Programming/Library/basic_template.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define fastIO ios_base::sync_with_stdio(0); cin.tie(0);  cout.tie(0);
+#define all(x) (x).begin(), (x).end()
 #define deb(x) cout << #x << " = " << x << endl
 #define deb2(x, y) cout << #x << " = " << x << "  ,  " << #y << "=" << y << endl
+typedef long long ll;
 
 int main() {
     fastIO;
